0a2783126b698ac2cfb67ea35bfc1ef8009556a6
konradbukanski <46792943+konradbukanski@users.noreply.github.com>
2019-01-24 16:04:44 +0100

    add css

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
 let expenses =0 
-
 
 document.querySelector('.calculate').addEventListener("click", (e) => {
     e.preventDefault();
@@ -19,32 +17,33 @@ document.querySelector('.calculate').addEventListener("click", (e) => {
     
     document.querySelector('.expenses input').value;
     const safings = document.querySelector('.safings input').value;
-
     let mouth = salary-expenses-safings;
     let day = (salary-expenses-safings)/30;
 
-    // const totalMouth = document.createElement('div');
-    document.querySelector(".mouth p").innerHTML = mouth;
-    
+    if(mouth<0){
+        document.querySelector(".mouth h1").innerHTML = "W tym miesiącu na minusie"; 
+        document.querySelector(".mouth p").innerHTML = `${mouth*(-1)} zł`;
+        document.querySelector(".day h1").innerHTML = "każdego dnia brakuje";
+        document.querySelector(".day p").innerHTML = `${(day*(-1)).toFixed(2)} zł`;
+    }else{
+        document.querySelector(".mouth h1").innerHTML = "Możesz wydać w tym miesiącu:"; 
+        document.querySelector(".mouth p").innerHTML = `${mouth} zł`;
+        document.querySelector(".day h1").innerHTML = "Na dzień przypada:";
+        document.querySelector(".day p").innerHTML = `${day.toFixed(2)} zł`;
+    }
 
-
-    // totalMouth.innerHTML = mouth;
-
-    // const totalDay = document.createElement('div');
-    document.querySelector(".day p").innerHTML = day;
-        
-    // totalDay.innerHTML = day;
 })
 
 document.querySelector('.newExpens').addEventListener("click", (e) => {
-    console.log("click");
     const newExpens = document.createElement("p")
-    newExpens.innerHTML = document.querySelector('.newExpensText').value;
     const newExpensInput = document.createElement("INPUT");
     newExpensInput.setAttribute("type", "number");
     newExpensInput.setAttribute("value", "0");
     newExpensInput.setAttribute("min", "0");
     newExpens.appendChild(newExpensInput);
     document.querySelector(".expenses").appendChild(newExpens);
+    const newExpensText = document.createElement("span");
+    newExpensText.innerHTML = ` zł - ${document.querySelector('.newExpensText').value}`;
+    newExpens.appendChild(newExpensText);
     document.querySelector('.newExpensText').value ="";
 })
